cb553965cc828e433bdc80bc72436d14f6bba6be
Rohit Kumar Singh <33059878+singhrohit487@users.noreply.github.com>
2021-05-12 18:38:53 +0800

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/singhrohit487/",
+    "name": "Rohit Kumar Singh",
+    "role": "DevOps Engineer",
+    "linkedinId":"https://www.linkedin.com/in/rohit-kumar-singh-64270138/",
+    "skypeid": "rajputrohit91",
+    "roleDescription": " I like dabbling in various parts of backend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rohit-kumar-singh-64270138/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/singhrohit487/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/rajputrohit91",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a DevOps Engineer at NTT Data, Singapore. I am a self taught DevOps/Backend Engineer, currently diving more deeper into DevOps and Cloud . I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Singapore",
+    "website":"https://github.com/singhrohit487",
+    "education":[
+      {
+        "UniversityName":"UPTU, Lucknow",
+        "specialization":"Computer Science & Engineering",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2013",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
